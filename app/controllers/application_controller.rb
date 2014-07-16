fefe44a1a3d4e55f161e7a5844a327e76c47f41b
odillem2 <odillem2@upmc.edu>
2014-07-16 11:05:52 -0400

    Add naive solution
    - Add algorithm that iterates the dictionary text file word-by-word
      and finds all matches
    
    - Filters on a regex, if provided

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,44 @@
 class ApplicationController < ActionController::API
+
+  def unscrabble
+    if params[:rack].present?
+      results = solve( params[:rack], params[:regex] )
+      render json: { results: results }
+    else
+      render json: { error: I18n.t('error.rack_required') }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def solve( rack, regex )
+    matches = []
+
+    IO.foreach UnscrabbleApi::Application.config.dictionary_path do |word|
+      word = word.chomp.downcase
+      temp_rack = rack
+
+      begin
+        word.chars.each do |letter|
+          if temp_rack =~ /#{letter}/
+            temp_rack = temp_rack.sub( letter, '' )
+          elsif temp_rack =~ /\*/
+            temp_rack = temp_rack.sub( '*', '' )
+          else
+            raise 'Not a match'
+          end
+        end
+
+        if regex
+          matches << word if word =~ /#{regex}/
+        else
+          matches << word
+        end
+      rescue
+      end
+    end
+
+    matches
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module UnscrabbleApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.dictionary_path = "#{Rails.root}/enable1.txt"
   end
 end
