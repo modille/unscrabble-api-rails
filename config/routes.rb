2807617a7f9bc7abeccb5c7a63c8cbfacce3d729
odillem2 <odillem2@upmc.edu>
2014-07-16 11:20:25 -0400

    Change from GET to POST

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'unscrabble' => 'application#unscrabble'
+  post 'unscrabble' => 'application#unscrabble'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
